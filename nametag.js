9f9c72ff789e42941e64799c4a4467b12f1f9bd2
ClareMcDonald <clare.s.mcdonald@gmail.com>
2021-11-04 11:13:49 -0700

    work on nametag

---
+++ nametag.js
@@ -0,0 +1,29 @@
+/*Input
+Why? This tells us what words we will add to the box.
+How?
+gives an input element, which grabs whatever words the user has typed */
+const inputEl = document.getElementById('input');
+const buttonEl = document.getElementById('button');
+const boxEl = document.getElementById('box');
+
+const blueButton = document.getElementById('blue');
+const grayButton = document.getElementById('gray');
+const greenButton = document.getElementById('green');
+
+
+/*Why? This tells us when we add to the box.
+How? */
+
+    // in here, do stuff
+buttonEl.addEventListener('click', () => {
+    boxEl.textContent = inputEl.value;
+    inputEl.value = '';
+})
+
+
+/* Some other div
+Why? This is the box we'll inject words into.
+How? */
+
+
+
